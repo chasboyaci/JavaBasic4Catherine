301cfa99cb7964345e32d174257947780285badd
chasboyaci <catherine.hasboyaci@gmail.com>
2022-12-04 11:15:59 -0500

    notes

---
+++ src/com/neotech/lesson33/WriteIntoExcel.java
@@ -0,0 +1,48 @@
+package com.neotech.lesson33;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class WriteIntoExcel {
+
+	public static void main(String[] args) throws IOException {
+		
+		
+		String filePath = System.getProperty("user.dir") + "/test_data/Test.xlsx";
+		
+		
+		FileInputStream fis = new FileInputStream(filePath);
+		
+		Workbook b = new XSSFWorkbook(fis);
+		Sheet s = b.getSheet("TestData");
+		
+		//We want to add a new Header "Country"
+		s.getRow(0).createCell(5).setCellValue("Country");
+		
+		//to add values below Country, we just change the row number
+		
+		
+		
+		//We want to create a row, 
+		s.createRow(3).createCell(0).setCellValue("Fatihcan");
+		
+		//to add a new cell on the row we just created
+		s.getRow(3).createCell(1).setCellValue("Dogan");
+		
+		//We want to create a new sheet
+		b.createSheet("Arin");
+		
+		//to write into a file, we need a fos
+		FileOutputStream fos = new FileOutputStream(filePath);
+		
+		b.write(fos);
+
+	}
+
+}
